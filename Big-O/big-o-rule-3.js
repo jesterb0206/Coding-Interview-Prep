857e3a151b1068a33de0866714d77ddc1a66ad09
jesterb0206 <jesterb@seattleu.edu>
2023-05-19 22:17:03 -0700

    Big O Rule 3

---
+++ Big-O/big-o-rule-3.js
@@ -0,0 +1,11 @@
+function compressBoxesTwice(boxes, boxes2) {
+  boxes.forEach(function (boxes) {
+    console.log(boxes);
+  });
+
+  boxes2.forEach(function (boxes) {
+    console.log(boxes);
+  });
+}
+
+// O(a + b)
