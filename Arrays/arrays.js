a90fedab69851ed7736a99bae9f226cd94f412f1
jesterb0206 <jesterb@seattleu.edu>
2023-05-24 18:14:01 -0700

    Merge Sorted Arrays

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -153,3 +153,37 @@ const reverse3 = (str) => [...str].reverse().join('');
 reverse('Bradley');
 reverse2('Rosalinda');
 reverse3('Prince');
+
+// Merge Sorted Arrays
+
+function mergeSortedArrays(array1, array2) {
+  const mergedArray = [];
+
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+
+  if (array1.length === 0) {
+    return array2;
+  }
+
+  if (array2.length === 0) {
+    return array1;
+  }
+
+  while (array1Item || array2Item) {
+    if (array2Item === undefined || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+  return mergedArray;
+}
+
+mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]);
