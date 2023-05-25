077e0f594df5a03e4b7e47b72cd85572a698083b
jesterb0206 <jesterb@seattleu.edu>
2023-05-24 17:34:02 -0700

    Implementing An Array

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -88,3 +88,45 @@ class Wizard extends Player {
 
 const wizard1 = new Wizard('Shelly', 'Healer');
 const wizard2 = new Wizard('Shawn', 'Dark Magic');
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const newArray = new MyArray();
+newArray.push('Hi!');
+newArray.pop();
+console.log(newArray);
