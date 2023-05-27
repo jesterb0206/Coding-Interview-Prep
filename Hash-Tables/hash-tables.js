c69ff86f8be7cec017c8778d9a24eeec78cc8094
jesterb0206 <jesterb@seattleu.edu>
2023-05-26 21:13:01 -0700

    Implementing A Hash Table

--- Hash-Tables/hash-tables.js
+++ Hash-Tables/hash-tables.js
@@ -10,3 +10,23 @@ let user = {
 user.age; // O(1)
 user.spell = 'abracadabra'; // O(1)
 user.scream(); // O(1)
+
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+myHashTable.get('grapes');
+myHashTable.set('apples', 9);
+myHashTable.get('apples');
