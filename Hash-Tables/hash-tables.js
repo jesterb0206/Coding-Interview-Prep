c8d6d44a51a5481d3e30b5acfa16bb60333f3415
jesterb0206 <jesterb@seattleu.edu>
2023-05-26 22:40:01 -0700

    First Recurring Character

--- Hash-Tables/hash-tables.js
+++ Hash-Tables/hash-tables.js
@@ -72,3 +72,28 @@ myHashTable.get('grapes');
 myHashTable.set('apples', 9);
 myHashTable.get('apples');
 myHashTable.keys();
+
+function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+}
+
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+  }
+  return undefined;
+}
+
+firstRecurringCharacter2([1, 5, 5, 1, 3, 4, 6]);
