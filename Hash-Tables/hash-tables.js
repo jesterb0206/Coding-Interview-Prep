ccab08554371ac41e21ff434c0b5ba7ee8578a8e
jesterb0206 <jesterb@seattleu.edu>
2023-05-26 22:06:02 -0700

    Implementing A Hash Table Part 2

--- Hash-Tables/hash-tables.js
+++ Hash-Tables/hash-tables.js
@@ -23,6 +23,28 @@ class HashTable {
     }
     return hash;
   }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key) {
+    const address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
 const myHashTable = new HashTable(50);
