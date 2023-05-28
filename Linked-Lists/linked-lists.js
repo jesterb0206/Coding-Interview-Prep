a6526fd34233d3bed085a8646d046f62760d0878
jesterb0206 <jesterb@seattleu.edu>
2023-05-27 19:01:01 -0700

    append()

--- Linked-Lists/linked-lists.js
+++ Linked-Lists/linked-lists.js
@@ -7,9 +7,20 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
-  append(value) {}
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {}
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+myLinkedList.prepend(1);
