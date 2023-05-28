2b8440752e6c93e49ede9ab1a6bb25e1ed4a9f7f
jesterb0206 <jesterb@seattleu.edu>
2023-05-28 01:16:04 -0700

    insert()

--- Trees/trees.js
+++ Trees/trees.js
@@ -11,7 +11,29 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(value) {}
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
 
   lookup(value) {}
 }
