0f54bd74734baac2ddc576ce1b42257281aee4c7
jesterb0206 <jesterb@seattleu.edu>
2023-05-27 19:47:01 -0700

    insert()

--- Linked-Lists/linked-lists.js
+++ Linked-Lists/linked-lists.js
@@ -7,6 +7,7 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
   append(value) {
     const newNode = {
       value: value,
@@ -17,6 +18,7 @@ class LinkedList {
     this.length++;
     return this;
   }
+
   prepend(value) {
     const newNode = {
       value: value,
@@ -27,6 +29,7 @@ class LinkedList {
     this.length++;
     return this;
   }
+
   printList() {
     const array = [];
     let currentNode = this.head;
@@ -36,12 +39,40 @@ class LinkedList {
     }
     return array;
   }
+
   insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
     return this.printList();
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
   }
 }
 
 let myLinkedList = new LinkedList(10);
+
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
