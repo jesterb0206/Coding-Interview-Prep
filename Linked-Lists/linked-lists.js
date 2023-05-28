aac7c2ff2f042ffd566265c403ccb21444b5bcda
jesterb0206 <jesterb@seattleu.edu>
2023-05-27 19:26:01 -0700

    printList()

--- Linked-Lists/linked-lists.js
+++ Linked-Lists/linked-lists.js
@@ -27,9 +27,23 @@ class LinkedList {
     this.length++;
     return this;
   }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    return this.printList();
+  }
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
