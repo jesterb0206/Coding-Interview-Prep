93514ab4999c6bc13262367e7f2201ed64b82add
jesterb0206 <jesterb@seattleu.edu>
2023-05-28 00:04:02 -0700

    Stack Implementation (Array)

--- Stacks-Queues/stacks-queues.js
+++ Stacks-Queues/stacks-queues.js
@@ -5,41 +5,61 @@ class Node {
   }
 }
 
+// class Stack {
+//   constructor() {
+//     this.top = null;
+//     this.bottom = null;
+//     this.length = 0;
+//   }
+
+//   peek() {
+//     return this.top;
+//   }
+
+//   push(value) {
+//     const newNode = new Node(value);
+//     if (this.length === 0) {
+//       this.top = newNode;
+//       this.bottom = newNode;
+//     } else {
+//       const holdingPointer = this.top;
+//       this.top = newNode;
+//       this.top.next = holdingPointer;
+//     }
+//     this.length++;
+//     return this;
+//   }
+
+//   pop() {
+//     if (!this.top) {
+//       return null;
+//     }
+//     if (this.top === this.bottom) {
+//       this.bottom = null;
+//     }
+//     const holdingPointer = this.top;
+//     this.top = this.top.next;
+//     this.length--;
+//     return this;
+//   }
+// }
+
 class Stack {
   constructor() {
-    this.top = null;
-    this.bottom = null;
-    this.length = 0;
+    this.array = [];
   }
 
   peek() {
-    return this.top;
+    return this.array[this.array.length - 1];
   }
 
   push(value) {
-    const newNode = new Node(value);
-    if (this.length === 0) {
-      this.top = newNode;
-      this.bottom = newNode;
-    } else {
-      const holdingPointer = this.top;
-      this.top = newNode;
-      this.top.next = holdingPointer;
-    }
-    this.length++;
+    this.array.push(value);
     return this;
   }
 
   pop() {
-    if (!this.top) {
-      return null;
-    }
-    if (this.top === this.bottom) {
-      this.bottom = null;
-    }
-    const holdingPointer = this.top;
-    this.top = this.top.next;
-    this.length--;
+    this.array.pop();
     return this;
   }
 }
