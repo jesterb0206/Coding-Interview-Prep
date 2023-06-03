98169a0fe9ef320e1779fe91b9d77385b832c489
jesterb0206 <jesterb@seattleu.edu>
2023-06-02 21:56:02 -0700

    Reverse a String With Recursion

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -34,3 +34,29 @@ function fibonacciRecursive(n) {
 }
 
 fibonacciRecursive(6);
+
+function reverseString(str) {
+  let arrayStr = str.split('');
+  let reversedArray = [];
+  function addToArray(array) {
+    if (array.length > 0) {
+      reversedArray.push(array.pop());
+      addToArray(array);
+    }
+    return;
+  }
+  addToArray(arrayStr);
+  return reversedArray.join('');
+}
+
+reverseString('Bradley Jester');
+
+function reverseStringRecursive(str) {
+  if (str === '') {
+    return '';
+  } else {
+    return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+  }
+}
+
+reverseStringRecursive('Bradley Jester');
