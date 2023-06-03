dca04f2b1384f4240d78980eb36514a53ba805ce
jesterb0206 <jesterb@seattleu.edu>
2023-06-02 21:45:01 -0700

    Fibonacci

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -15,3 +15,22 @@ function findFactorialRecursive(number) {
 
 findFactorialIterative(5);
 findFactorialRecursive(5);
+
+function fibonacciIterative(n) {
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
+}
+
+fibonacciIterative(3);
+
+function fibonacciRecursive(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+fibonacciRecursive(6);
