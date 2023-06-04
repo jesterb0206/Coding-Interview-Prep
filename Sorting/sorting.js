9300a42483347d58646ea27b6e2ee20b2928356a
jesterb0206 <jesterb@seattleu.edu>
2023-06-03 18:19:01 -0700

    Selection Sort

--- Sorting/sorting.js
+++ Sorting/sorting.js
@@ -16,3 +16,23 @@ function bubbleSort(array) {
 bubbleSort(numbers);
 
 console.log(numbers);
+
+function selectionSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    let min = i;
+    let temp = array[i];
+    for (let j = i + 1; j < length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+}
+
+selectionSort(numbers);
+
+console.log(numbers);
