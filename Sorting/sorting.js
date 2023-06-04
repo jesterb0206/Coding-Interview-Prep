3b51a34f95fec38f523e269d77a0b7de15c4230b
jesterb0206 <jesterb@seattleu.edu>
2023-06-03 18:57:01 -0700

    Quick Sort

--- Sorting/sorting.js
+++ Sorting/sorting.js
@@ -88,3 +88,39 @@ function merge(left, right) {
 const answer = mergeSort(numbers);
 
 console.log(answer);
+
+function quickSort(array, left, right) {
+  const len = array.length;
+  let pivot;
+  let partitionIndex;
+  if (left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+}
+
+function partition(array, pivot, left, right) {
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivotValue) {
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(array, firstIndex, secondIndex) {
+  var temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+}
+
+quickSort(numbers, 0, numbers.length - 1);
+
+console.log(numbers);
