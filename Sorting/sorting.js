09efbaa16fcf28e872626c91eecf4d01fb72969a
jesterb0206 <jesterb@seattleu.edu>
2023-06-03 18:28:02 -0700

    Insertion Sort

--- Sorting/sorting.js
+++ Sorting/sorting.js
@@ -36,3 +36,24 @@ function selectionSort(array) {
 selectionSort(numbers);
 
 console.log(numbers);
+
+function insertionSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    if (array[i] < array[0]) {
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      if (array[i] < array[i - 1]) {
+        for (var j = 1; j < i; j++) {
+          if (array[i] >= array[j - 1] && array[i] < array[j]) {
+            array.splice(j, 0, array.splice(i, 1)[0]);
+          }
+        }
+      }
+    }
+  }
+}
+
+insertionSort(numbers);
+
+console.log(numbers);
