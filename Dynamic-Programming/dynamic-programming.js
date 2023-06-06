639a75198d71745aa0c47322550e7335b6e77113
jesterb0206 <jesterb@seattleu.edu>
2023-06-05 23:12:01 -0700

    Implementing Dynamic Programming

--- Dynamic-Programming/dynamic-programming.js
+++ Dynamic-Programming/dynamic-programming.js
@@ -21,3 +21,44 @@ const memoized = memoizeAddTo80();
 
 console.log(1, memoized(6));
 console.log(2, memoized(6));
+
+let calculations = 0;
+
+function fibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function fibonacciMaster() {
+  let cache = {};
+  return function fib(n) {
+    calculations++;
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+      }
+    }
+  };
+}
+
+function fibonacciMaster2(n) {
+  let answer = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    answer.push(answer[i - 2] + answer[i - 1]);
+  }
+  return answer.pop();
+}
+
+const fasterFib = fibonacciMaster();
+
+console.log('Slow', fibonacci(35));
+console.log('Dynamic Programming', fasterFib(100));
+console.log('Dynamic Programming 2', fibonacciMaster2(100));
+console.log('We did ' + calculations + ' calculations!');
